df054a5b6a5d17cd3ff8193b7ec2c0c2ad991803
kundamanasranjan15@gmail.com <kundamanasranjan15@gmail.com>
2023-01-18 12:19:40 +0000

    Jinja Pivot

--- models/marts/core/int_orders_pivoted.sql
+++ models/marts/core/int_orders_pivoted.sql
@@ -1,20 +1,22 @@
+{%- set payment_methods = ['bank_transfer','coupon','credit_card','gift_card'] -%}
 with
-    payments as {select * from {{ ref('stg_payment') }}},
+    payments as 
+    (
+        select * from {{ ref('stg_payment') }}
+    ),
 
-    pivoted as {
+    pivoted as (
         select
             order_id,
-
-            {% set payment_methods = ['bank_transfer','coupon','credit_card','gift_card'] %}
-
-            {% for payment_method in payment_methods %}
-
-            sum(case when payment_method = {{payment_method}} then amount else 0 end) as {{payment_method}}_amount
-
-            {% endfor %}
+            {% for payment_method in payment_methods -%}
+            sum(case when payment_method = '{{payment_method}}' then amount else 0 end) as {{payment_method}}_amount
+            {%- if not loop.last -%}
+             ,
+            {%- endif %}
+            {% endfor -%}
         from payments
         where status = 'success'
         group by 1
-    }
+    )
 
 select * from pivoted
