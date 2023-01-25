990de1fcbbb3231f530c49bbd586f7662617963f
kundamanasranjan15@gmail.com <kundamanasranjan15@gmail.com>
2023-01-25 06:07:13 +0000

    Final commit

--- macros/union_tables_by_prefix.sql
+++ macros/union_tables_by_prefix.sql
@@ -1,6 +1,6 @@
 {%- macro union_tables_by_prefix(database, schema, prefix) -%}
 
-  {%- set tables = dbt_utils.get_relations_by_prefix('database', 'schema', 'prefix') -%}
+  {%- set tables = dbt_utils.get_relations_by_prefix(database=database, schema=schema, prefix=prefix) -%}
 
   {% for table in tables %}
 
